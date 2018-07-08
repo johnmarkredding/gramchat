4c576b865224d1f5aa6c93305137f4ce31aebcb3
John Mark Redding <johomurk@gmail.com>
2018-07-08 08:51:33 -0500

    Add ES6 features.

--- public/js/client.js
+++ public/js/client.js
@@ -4,15 +4,15 @@
 
 $(function () {
 // Connect to Socket.io server
-	var socket = io();
+	let socket = io();
 	$('form').submit(function formSubmit() {
 		socket.emit('chat message', $('#message').val());
 		$('#message').val('');
 		return false;
 	});
 	socket.on('chat message', function(msg, name) {	
-		var newMessage = document.createElement('li');
-		newMessage.innerHTML = '<h3>' + name + '</h3>' + msg;
+		let newMessage = document.createElement('li');
+		newMessage.innerHTML = `<h3>${name}</h3>${msg}`;
 		$('#messages').append(newMessage);
 		window.scrollTo(0,document.body.scrollHeight);
 	});
